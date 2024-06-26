1408d1f45ac45b48c0b4cfc0ef22b7b38168c68d
Edmondo Porcu <edmondo.porcu@capitalone.com>
2024-06-26 08:13:09 -0400

    Saving local changes for capital one

--- nvim/.config/nvim/lua/custom/plugins/which-key.lua
+++ nvim/.config/nvim/lua/custom/plugins/which-key.lua
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:nvim/.config/nvim/lua/custom/plugins/which-key.lua
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
@@ -6,3 +7,19 @@ return {
     vim.o.timeoutlen = 300
   end,
 }
+||||||| parent of 393eb50 (Saving local changes for capital one):nvim/.config/nvim/lua/custom/plugins/which-key.lua
+=======
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
+>>>>>>> 393eb50 (Saving local changes for capital one):nvim/.config/nvim/lua/plugins/which-key.lua

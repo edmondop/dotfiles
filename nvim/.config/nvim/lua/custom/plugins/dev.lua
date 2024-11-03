8010b08cd95db924232f30c19913a176c0d221cf
Edmondo Porcu <edmondo.porcu@gmail.com>
2024-11-03 06:51:03 -0500

    Refactoring dev

--- nvim/.config/nvim/lua/custom/plugins/dev.lua
+++ nvim/.config/nvim/lua/custom/plugins/dev.lua
@@ -4,7 +4,12 @@ local langs = require("custom.plugins.dev.langs")
 local lsp = require("custom.plugins.dev.lsp")
 local parsing = require("custom.plugins.dev.parsing")
 local productivity = require("custom.plugins.dev.productivity")
-local remoting = require("custom.plugins.dev.productivity")
+local remoting = require("custom.plugins.dev.remoting")
 local snippets = require("custom.plugins.dev.snippets")
 
-return vim.tbl_deep_extend("force", ai, dap, langs, lsp, parsing, productivity, remoting, snippets)
+local result = {}
+for _, tbl in ipairs({ ai, dap, langs, lsp, parsing, productivity, remoting, snippets }) do
+	vim.list_extend(result, tbl)
+end
+
+return result
--- zsh/shell/go.sh
+++ zsh/shell/go.sh
@@ -1,4 +1,16 @@
+# Set GoEnv root directory
 export GOENV_ROOT="$HOME/.goenv"
+
+# Add GoEnv to PATH
 export PATH="$GOENV_ROOT/bin:$PATH"
-# GoLang
+
+# Initialize GoEnv
 eval "$(goenv init -)"
+
+# Set GOROOT and GOPATH
+export GOROOT="$(go env GOROOT)"
+export GOPATH="$HOME/go"
+
+# Add GOROOT and GOPATH to PATH
+export PATH="$GOROOT/bin:$PATH"
+export PATH="$GOPATH/bin:$PATH"

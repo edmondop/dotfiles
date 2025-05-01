ae2e1dddfd0689a61f934a989395fe04cc8bb463
Edmondo Porcu <eporcu@pinterest.com>
2025-05-01 13:36:00 +0000

    Adding Ubuntu support

--- zsh/shell/aliases.sh
+++ zsh/shell/aliases.sh
@@ -2,4 +2,8 @@
 alias glp='git log --pretty=format:"%C(yellow)%h%Creset - %C(green)%an%Creset, %ar : %s"'
 alias ls=colorls
 alias vim=nvim
-alias cat=bat
+if which batcat >/dev/null 2>&1; then
+    alias cat=batcat
+elif which bat >/dev/null 2>&1; then
+    alias cat=bat
+fi

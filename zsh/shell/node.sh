c42135951fea59adb206c1814a05cf4e457c1c3f
Edmondo Porcu <eporcu@pinterest.com>
2025-05-02 20:53:52 +0000

    Aadding also pnpm support

--- zsh/shell/node.sh
+++ zsh/shell/node.sh
@@ -4,6 +4,12 @@
 # fnm
 FNM_PATH="$HOME/.local/share/fnm"
 if [ -d "$FNM_PATH" ]; then
-	export PATH="$HOME/.local/share/fnm:$PATH"
-	eval "$(fnm env)"
+    export PATH="$HOME/.local/share/fnm:$PATH"
+    eval "$(fnm env)"
 fi
+
+export PNPM_HOME="/home/eporcu/.local/share/pnpm"
+case ":$PATH:" in
+*":$PNPM_HOME:"*) ;;
+*) export PATH="$PNPM_HOME:$PATH" ;;
+esac

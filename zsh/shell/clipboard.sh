76fdb2471fe85c87a79b7ce59ed44f11a9678820
Edmondo Porcu <eporcu@pinterest.com>
2025-05-04 20:26:07 +0000

    Extended iTerm2 support + various others

--- zsh/shell/clipboard.sh
+++ zsh/shell/clipboard.sh
@@ -2,24 +2,25 @@
 # Add this to your .bashrc or .zshrc
 
 copy() {
-	# Check if it's macOS
-	if [[ "$(uname)" == "Darwin" ]]; then
-		# Use pbcopy on macOS
-		pbcopy
-	else
-		# If on Linux, check if Wayland or X11 is in use
-		if [ "$WAYLAND_DISPLAY" ]; then
-			# Use wl-copy if running on Wayland
-			wl-copy
-		elif [ "$DISPLAY" ]; then
-			# Use xclip if running on X11
-			xclip -selection clipboard
-		else
-			echo "No display server detected. Could not determine clipboard tool." >&2
-			return 1
-		fi
-	fi
+    # Check if it's macOS
+    if [[ "$(uname)" == "Darwin" ]]; then
+        # Use pbcopy on macOS
+        pbcopy
+    else
+        # If on Linux, check if Wayland or X11 is in use
+        if [ "$WAYLAND_DISPLAY" ]; then
+            # Use wl-copy if running on Wayland
+            wl-copy
+        elif [ "$DISPLAY" ]; then
+            # Use xclip if running on X11
+            xclip -selection clipboard
+        else
+            if ! command -v it2copy &>/dev/null; then
+                echo "Error: it2copy is not available ." >&2
+                return 1
+            fi
+            # Use it2copy, assuming this is is an iTerm2 remote ssh session
+            it2copy
+        fi
+    fi
 }
-
-# Example usage:
-# cat file | copy
---
+++ zsh/shell/iterm2-integration.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+if [ -f "$HOME/iterm2_shell_integration.bash" ]; then
+    # Source the file
+    source "$HOME/iterm2_shell_integration.bash"
+fi

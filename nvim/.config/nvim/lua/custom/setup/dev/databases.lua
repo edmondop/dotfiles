69b31732e40a1cd0e0ced70e7e9df5f356a46070
Edmondo Porcu <eporcu@pinterest.com>
2025-05-12 13:53:54 +0000

    Added support for filetype ql

--- nvim/.config/nvim/lua/custom/setup/dev/databases.lua
+++ nvim/.config/nvim/lua/custom/setup/dev/databases.lua
@@ -5,7 +5,15 @@ M = {}
 local setup_dadbot_ui = function()
 	vim.keymap.set("n", "<leader>D", "<CMD>DBUIToggle<CR>", { desc = "Databases : Toggle Dadbot UI" })
 end
+
+local setup_ql_filetype_remapping = function()
+	vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+		pattern = "*.ql",
+		command = "setfiletype sql",
+	})
+end
 M.setup = function()
 	setup_dadbot_ui()
+	setup_ql_filetype_remapping()
 end
 return M

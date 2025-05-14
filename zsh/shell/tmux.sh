88fc03ac9c335b74986fb65b0e40837bf4b30269
Edmondo Porcu <eporcu@pinterest.com>
2025-05-14 19:55:35 +0000

    Tmux + iterm2

--- zsh/shell/tmux.sh
+++ zsh/shell/tmux.sh
@@ -2,3 +2,12 @@
 
 export PATH="$HOME/.tmuxifier/bin:$PATH"
 eval "$(tmuxifier init -)"
+
+if test "$SSH_AUTH_SOCK"; then
+    if test -z "$TMUX"; then
+        if [[ $TERM_PROGRAM != "vscode" ]]; then
+            echo 'Linking ~/.ssh/ssh_auth_sock to $SSH_AUTH_SOCK'
+            ln -sf $SSH_AUTH_SOCK ~/.ssh/ssh_auth_sock
+        fi
+    fi
+fi

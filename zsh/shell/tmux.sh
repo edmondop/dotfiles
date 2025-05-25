0919924600e728e1b4fc74423606456b483bace4
Edmondo Porcu <eporcu@pinterest.com>
2025-05-25 13:42:35 +0000

    Always repair the SSH sock

--- zsh/shell/tmux.sh
+++ zsh/shell/tmux.sh
@@ -3,7 +3,8 @@
 export PATH="$HOME/.tmuxifier/bin:$PATH"
 eval "$(tmuxifier init -)"
 
-if [[ -S "$SSH_AUTH_SOCK" && ! -L "$SSH_AUTH_SOCK" ]]; then
-    ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+if [[ -S "$SSH_AUTH_SOCK" ]]; then
+    export OLD_SOCK="${OLD_SOCK:-$SSH_AUTH_SOCK}" # Save the old sock if not already saved
+    ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock  # Always update the symbolic link
 fi
 export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock

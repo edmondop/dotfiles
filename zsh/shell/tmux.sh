40c94b16e2beb2f5a18ab5b49979c3a885d859c0
Edmondo Porcu <eporcu@pinterest.com>
2025-06-03 15:08:48 +0000

    Fixing ssh auth sock

--- zsh/shell/tmux.sh
+++ zsh/shell/tmux.sh
@@ -11,5 +11,3 @@ if [[ "$(uname -s)" == "Linux" && "$(lsb_release -is 2>/dev/null)" == "Ubuntu" &
     fi
     export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
 fi
-
-Linux devrestricted-eporcu 6.8.0-1026-aws #28-Ubuntu SMP Mon Mar 24 19:32:19 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux

47cbdc874ab5ce0a55a60793405c7b55b7fa0565
Edmondo Porcu <eporcu@pinterest.com>
2025-06-08 20:49:18 +0000

    Fixed tmux config

--- zsh/shell/tmux.sh
+++ zsh/shell/tmux.sh
@@ -3,11 +3,33 @@
 export PATH="$HOME/.tmuxifier/bin:$PATH"
 eval "$(tmuxifier init -)"
 
-if [[ "$(uname -s)" == "Linux" && "$(lsb_release -is 2>/dev/null)" == "Ubuntu" && "$(hostname)" == "devrestricted-eporcu" ]]; then
-
-    if [[ -S "$SSH_AUTH_SOCK" ]]; then
-        export OLD_SOCK="${OLD_SOCK:-$SSH_AUTH_SOCK}" # Save the old sock if not already saved
-        ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock  # Always update the symbolic link
-    fi
+update_symlink_and_export() {
+    local socket="$1"
+    ln -sf "$socket" ~/.ssh/ssh_auth_sock
     export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+    echo "Updated SSH_AUTH_SOCK symlink to ${SSH_AUTH_SOCK}"
+}
+
+fix_ssh_agent_forwarding() {
+    ssh-add -l 1>/dev/null 2>/dev/null
+    if [[ "$?" != "0" ]]; then
+        local socket=$(find /tmp/ssh-**/agent.* | head -n1)
+        update_symlink_and_export "$socket"
+    fi
+}
+
+update_socket_symlink() {
+    if [[ "$SSH_AUTH_SOCK" != "$HOME/.ssh/ssh_auth_sock" ]]; then
+        if [[ -S "$SSH_AUTH_SOCK" ]]; then
+            update_symlink_and_export "$SSH_AUTH_SOCK"
+        else
+            echo "SSH_AUTH_SOCK is not a valid socket: $SSH_AUTH_SOCK"
+        fi
+    else
+        echo "SSH_AUTH_SOCK already points to the symlink: $SSH_AUTH_SOCK"
+    fi
+}
+
+if [[ "$(uname -s)" == "Linux" && "$(lsb_release -is 2>/dev/null)" == "Ubuntu" && "$(hostname)" == "devrestricted-eporcu" ]]; then
+    update_socket_symlink
 fi
